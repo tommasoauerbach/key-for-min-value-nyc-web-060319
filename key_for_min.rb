379d409f7a422674656df824b0fce6d3c6260db8
tommaso auerbach <tsa302@nyu.edu>
2019-06-17 19:16:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,3 +1,14 @@
 def key_for_min_value(name_hash)
-  
-end
+  min_value = nil
+    min_key = nil
+    name_hash.each do |k, v|
+      if min_value == nil
+        min_value = v
+        min_key = k
+      elsif min_value > v
+        min_value = v
+        min_key = k
+      end
+    end
+    min_key
+  end
